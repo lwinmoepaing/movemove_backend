21b6b4cd2ce0611652e6450dd776e4fdc66e420a
Lwin Moe Paing <lwinmoepaing@marathonmyanmar.com>
2020-09-11 08:34:03 +0630

    Init App

---
+++ services/logger.js
@@ -0,0 +1,44 @@
+const morgan = require('morgan')
+const path = require('path')
+const fs = require('fs')
+const moment = require('moment-timezone')
+
+const accessLogStream = (fileName) => fs.createWriteStream(
+	path.join(__dirname, '../', 'logs', `${fileName}`),
+	{ flags: 'a' },
+)
+
+morgan.token('date', () => {
+	return moment().tz('Asia/Yangon').format()
+})
+
+morgan.token('user', (req) => {
+	return  req.user ? `UserId:${req.user._id} Name:"${req.user.name}"`: '"Guest"'
+})
+
+morgan.format('loggerCustomFormat', '[:date[clf]] :method ":url", Status :status, ContentLength :res[content-length] - :response-time ms, :user')
+
+module.exports = morgan('loggerCustomFormat', {
+	format: 'default',
+	stream: accessLogStream('access.log'),
+	skip: function (req, res) { return res.statusCode > 400 }
+})
+
+/**
+ * Not Found Log'll write on 404.log
+ *
+ */
+module.exports.notFoundLog = morgan('loggerCustomFormat', {
+	format: 'default',
+	stream: accessLogStream('404.log'),
+	skip: function (req, res) { return res.statusCode !== 404 }
+})
+
+/**
+ * Output Only Error Logs.
+ */
+module.exports.errorLog = morgan('loggerCustomFormat', {
+	format: 'default',
+	stream: accessLogStream('error.log'),
+	skip: function (req, res) { return res.statusCode < 400 && res.statusCode !== 404 }
+})
